9ab24afea842e2b4ca0be018c47b89d237147ba9
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2024-04-20 23:52:26 +0100

    Create Router.php

---
+++ src/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JPI\CRUD;
+
+use JPI\HTTP\Router as BaseRouter;
+use JPI\HTTP\Request;
+use JPI\HTTP\Response;
+
+/**
+ * Extended router with default error handling for 404 & 405.
+ */
+class Router extends BaseRouter {
+
+    protected function __construct(protected Request $request) {
+        $this->notFoundHandler = function (Request $request) {
+            return Response::json(404, [
+                "message" => "Unrecognised URI ({$request->getPath()}).",
+            ]);
+        };
+        $this->methodNotAllowedHandler = function (Request $request) {
+            return Response::json(405, [
+                "message" => "Method {$request->getMethod()} not allowed on {$request->getPath()}.",
+            ]);
+        };
+    }
+}
