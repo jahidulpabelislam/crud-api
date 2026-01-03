a31a9da5fbc6953fdf9e6cba9c0fccddc08d1e70
Jahidul Pabel Islam <jahidul@d3r.com>
2026-01-03 16:24:52 +0000

    Reorder routes

--- src/Router.php
+++ src/Router.php
@@ -29,11 +29,10 @@ class Router extends BaseRouter {
     public function addCRUDRoutes(string $basePath, string $controller, ?string $name = null): void {
         $basePath = trim($basePath, "/");
 
+        $this->addRoute("/$basePath/", "GET", "$controller::index");
+        $this->addRoute("/$basePath/", "POST", "$controller::create");
         $this->addRoute("/$basePath/{id}/", "GET", "$controller::read", $name);
         $this->addRoute("/$basePath/{id}/", "PUT", "$controller::update");
         $this->addRoute("/$basePath/{id}/", "DELETE", "$controller::delete");
-
-        $this->addRoute("/$basePath/", "GET", "$controller::index");
-        $this->addRoute("/$basePath/", "POST", "$controller::create");
     }
 }
