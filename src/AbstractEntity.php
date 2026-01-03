7d5a5d2ea028d4b2d1b9af0b8c17c129410c5211
Jahidul Pabel Islam <jahidul@d3r.com>
2026-01-03 19:10:51 +0000

    Rename item variables -> entity

--- src/AbstractEntity.php
+++ src/AbstractEntity.php
@@ -58,12 +58,12 @@ abstract class AbstractEntity extends BaseEntity {
                     continue;
                 }
 
-                $items = $value;
+                $entities = $value;
                 $value = [];
-                foreach ($items as $item) {
-                    $itemResponse = $item->getAPIResponse($depth + 1, $this);
-                    $itemResponse["_links"] = $item->getAPILinks();
-                    $value[] = $itemResponse;
+                foreach ($entities as $entity) {
+                    $entityResponse = $entity->getAPIResponse($depth + 1, $this);
+                    $entityResponse["_links"] = $entity->getAPILinks();
+                    $value[] = $entityResponse;
                 }
             }
             else if ($value instanceof DateTime) {
